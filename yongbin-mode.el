519e7543cb5e245c03251c3efc0f27bd313e337b
Hyungsuk Hong <aanoaa@gmail.com>
2016-06-07 17:54:06 +0900

    Rename yongbinize yongbin-swap

--- yongbin-mode.el
+++ yongbin-mode.el
@@ -8,15 +8,15 @@
         (if (null arg) (not yongbin-mode)
           (> (prefix-numeric-value arg) 0)))
   (if yongbin-mode
-      (add-hook 'pre-command-hook 'yongbinize)
-    (remove-hook 'pre-command-hook 'yongbinize)))
+      (add-hook 'pre-command-hook 'yongbin-swap)
+    (remove-hook 'pre-command-hook 'yongbin-swap)))
 
 (if (not (assq 'yongbin-mode minor-mode-alist))
     (setq minor-mode-alist
           (cons '(yongbin-mode " Yongbin")
                 minor-mode-alist)))
 
-(defun yongbinize ()
+(defun yongbin-swap ()
   "Insert the character you type, but swap ㅔ and ㅐ."
   (when (and (characterp last-command-event)
              (or (yongbin-e-or-ae-p last-command-event)
